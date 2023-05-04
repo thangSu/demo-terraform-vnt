1a85e01775a7fbfd45a29e82833757df81e82051
thangSu <phamthang3003@gmail.com>
2023-05-04 08:26:28 +0700

    add gitignore and aws provider & config

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+    region = "ap-southeast-1"
+}
+terraform {
+    backend "s3" {
+        bucket = "dev-terraform-state-vt"
+        key = "states/terraform.tfstate"
+        region = "ap-southeast-1"
+    }
+
+    required_providers {
+        aws = {
+            version = "4.63.0"
+            source  = "hashicorp/aws"
+        }
+    }
+}
